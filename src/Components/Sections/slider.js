2a5944fd7ee8f1cab6b4f4fb0d23932af470b6ef
Tav-115 <33943446+Tav-115@users.noreply.github.com>
2023-02-07 00:53:12 -0300

    7-2-23

--- src/Components/Sections/slider.js
+++ src/Components/Sections/slider.js
@@ -3,33 +3,30 @@ import RecursosCard from "./Recursos-card"
 const Slider = () => (
      <div className="container-slider">
         <div className="slider " id="slider">
-                
+
+        <div className="slider--section box-shadow">         
+                <RecursosCard nombre="Manual Seguridad Vacunas" 
+                subt="Febrero 2023"
+                image="https://drive.google.com/uc?export=download&id=13Vh-lGDrAk0DaulXshbl3P6a4I3tDhRU"
+                link="https://drive.google.com/file/d/13fYdVpalmZOKPNqvBxOS-KpMQF9F99it/view?usp=sharing"/>
+        </div>
+        <div className="slider--section box-shadow">         
+                <RecursosCard nombre="Manual Pfizer Bivariante" 
+                subt="(Adultos) Enero 2023"
+                image="https://drive.google.com/uc?export=download&id=10xj28_FwE_GosXMzB0M4x-98BlWRB_pv"
+                link="https://drive.google.com/file/d/10V80zkZ5Jl3iSq--uD8AFmtVtjqsmCnb/view?usp=sharing"/>
+        </div>
+        <div className="slider--section box-shadow">         
+                <RecursosCard nombre="Manipulación Pfizer Bivalente" 
+                subt="(Adultos) Enero 2023"
+                image="https://drive.google.com/uc?export=download&id=1c1obBVkKGlKm7aCvGezzD4i-KsC4Sb99"
+                link="https://drive.google.com/file/d/1bdXdXmQJ9UVxzvasa3iTKms1nlPTgxSO/view?usp=sharing"/>
+        </div>                
         <div className="slider--section box-shadow">
                 <RecursosCard nombre="Manual Almacenamiento de Vacunas" 
                 subt="Nivel Operativo"
                 image="https://drive.google.com/uc?export=download&id=11F8IyTl-P2iO4zfSU-gkX0kQll2GQVIc" 
                 link="https://drive.google.com/file/d/1USMUMsyP14fu9omKNSftyfO7n6myQziL/view?usp=sharing"/> 
-        </div>
-
-        <div className="slider--section box-shadow">         
-                <RecursosCard nombre="Memo uso Mod Pediátrica" 
-                subt="Dosis/Grupo poblacional"
-                image="https://drive.google.com/uc?export=download&id=1v0hUjwWRpb1duPcGEAIVggIDFd1ATtrh"
-                link="https://drive.google.com/file/d/1-kyniBH7hcHXapYnP5EBJtzfQGtFAlXu/view?usp=sharing"/>
-        </div>
-
-        <div className="slider--section box-shadow">         
-                <RecursosCard nombre="Circular Refuerzos" 
-                subt="6 Meses a 17 años"
-                image="https://drive.google.com/uc?export=download&id=1-hnxlJpeDEoJ797qgqAKvUuKF9lVxu7s"
-                link="https://drive.google.com/file/d/1F_g0Ud3AKaMCinbOCp1jBP9QYUPOb4DS/view?usp=sharing"/>
-        </div>
-
-        <div className="slider--section box-shadow">
-                <RecursosCard nombre="Extensión Pfizer" 
-                subt="Pediátrica 30-12-22"
-                image="https://drive.google.com/uc?export=download&id=14ft4hh704iJiWPg83jzMxq8s_gbsjWmM" 
-                link="https://drive.google.com/file/d/1PZV0jPzxQHTh8hNJpEXBvEDJ5-EYuD6T/view?usp=sharing"/> 
         </div>
 
 </div>
