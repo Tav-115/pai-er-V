afc798d690d167ad374a6b9a72ad1244c0ab862f
Tav-115 <33943446+Tav-115@users.noreply.github.com>
2023-02-22 05:58:29 -0300

    22-2-23

--- src/Components/Sections/slider.js
+++ src/Components/Sections/slider.js
@@ -5,28 +5,28 @@ const Slider = () => (
         <div className="slider " id="slider">
 
         <div className="slider--section box-shadow">         
+                <RecursosCard nombre="Manual Sputnik Ligth" 
+                subt="(Adultos) febrero 2023"
+                image="https://drive.google.com/uc?export=download&id=1XKy6Wc6eqk9u-g_eSBk6S-DvKFEROAdy"
+                link="https://drive.google.com/file/d/1rlf8AThX-uL07iR7y3SuovGUibpCWLqQ/view?usp=sharing"/>
+        </div>
+        <div className="slider--section box-shadow">         
+                <RecursosCard nombre="Almacenamiento Sputnik Ligth" 
+                subt=""
+                image="https://drive.google.com/uc?export=download&id=1dZVH8v7x-ROJxRY-AcW0Ims5QuvKSadi"
+                link="https://drive.google.com/file/d/11qggoSJgTk7gPhouBuQl5jKYalYQGQZ-/view?usp=sharing"/>
+        </div>                
+        <div className="slider--section box-shadow">
+                <RecursosCard nombre="Manual Spikevax Bivariante" 
+                subt="Moderna Pediátrica"
+                image="https://drive.google.com/uc?export=download&id=1A4NcZTe-VOjyPzXlOkBgZuj-HxIOOjCm" 
+                link="https://drive.google.com/file/d/1c34APdiDiCOXGWea00hXunlC-oxKqWKJ/view?usp=sharing"/> 
+        </div>
+        <div className="slider--section box-shadow">         
                 <RecursosCard nombre="Manual Seguridad Vacunas" 
                 subt="Febrero 2023"
                 image="https://drive.google.com/uc?export=download&id=13Vh-lGDrAk0DaulXshbl3P6a4I3tDhRU"
                 link="https://drive.google.com/file/d/13fYdVpalmZOKPNqvBxOS-KpMQF9F99it/view?usp=sharing"/>
-        </div>
-        <div className="slider--section box-shadow">         
-                <RecursosCard nombre="Manual Pfizer Bivariante" 
-                subt="(Adultos) Enero 2023"
-                image="https://drive.google.com/uc?export=download&id=10xj28_FwE_GosXMzB0M4x-98BlWRB_pv"
-                link="https://drive.google.com/file/d/10V80zkZ5Jl3iSq--uD8AFmtVtjqsmCnb/view?usp=sharing"/>
-        </div>
-        <div className="slider--section box-shadow">         
-                <RecursosCard nombre="Manipulación Pfizer Bivalente" 
-                subt="(Adultos) Enero 2023"
-                image="https://drive.google.com/uc?export=download&id=1c1obBVkKGlKm7aCvGezzD4i-KsC4Sb99"
-                link="https://drive.google.com/file/d/1bdXdXmQJ9UVxzvasa3iTKms1nlPTgxSO/view?usp=sharing"/>
-        </div>                
-        <div className="slider--section box-shadow">
-                <RecursosCard nombre="Manual Almacenamiento de Vacunas" 
-                subt="Nivel Operativo"
-                image="https://drive.google.com/uc?export=download&id=11F8IyTl-P2iO4zfSU-gkX0kQll2GQVIc" 
-                link="https://drive.google.com/file/d/1USMUMsyP14fu9omKNSftyfO7n6myQziL/view?usp=sharing"/> 
         </div>
 
 </div>
