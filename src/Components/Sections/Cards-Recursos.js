8937662c445542a53c44f4f6e6337fa2b06d8c5e
Tav-115 <33943446+Tav-115@users.noreply.github.com>
2023-02-22 21:19:58 -0300

    22-2-23

--- src/Components/Sections/Cards-Recursos.js
+++ src/Components/Sections/Cards-Recursos.js
@@ -296,6 +296,21 @@ const CardsRecursos = () => (
    <a name="Manuales" className="myanchor"></a>
       <h2 className='s-cols-2 m-cols-4 xl-cols-6'> Manuales y Guias</h2> 
 
+      <RecursosCard nombre="Manual Sputnik Ligth" 
+      subt="(Adultos) febrero 2023"
+      image="https://drive.google.com/uc?export=download&id=1XKy6Wc6eqk9u-g_eSBk6S-DvKFEROAdy"
+      link="https://drive.google.com/file/d/1rlf8AThX-uL07iR7y3SuovGUibpCWLqQ/view?usp=sharing"/>
+      
+      <RecursosCard nombre="Almacenamiento Sputnik Ligth" 
+      subt=""
+      image="https://drive.google.com/uc?export=download&id=1dZVH8v7x-ROJxRY-AcW0Ims5QuvKSadi"
+      link="https://drive.google.com/file/d/11qggoSJgTk7gPhouBuQl5jKYalYQGQZ-/view?usp=sharing"/>
+
+      <RecursosCard nombre="Manual Spikevax Bivariante" 
+      subt="Moderna Pediátrica"
+      image="https://drive.google.com/uc?export=download&id=1A4NcZTe-VOjyPzXlOkBgZuj-HxIOOjCm" 
+      link="https://drive.google.com/file/d/1c34APdiDiCOXGWea00hXunlC-oxKqWKJ/view?usp=sharing"/>
+
       <RecursosCard nombre="Manual Seguridad Vacunas" 
       subt="Febrero 2023"
       image="https://drive.google.com/uc?export=download&id=13Vh-lGDrAk0DaulXshbl3P6a4I3tDhRU"
