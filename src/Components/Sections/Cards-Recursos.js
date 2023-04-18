8a48f4a2155b5566e8926ef81703605787e346db
Tav-115 <33943446+Tav-115@users.noreply.github.com>
2023-04-17 23:40:07 -0300

    17/4/23

--- src/Components/Sections/Cards-Recursos.js
+++ src/Components/Sections/Cards-Recursos.js
@@ -427,6 +427,26 @@ const CardsRecursos = () => (
       <a name="Circulares" className="myanchor"></a>
       <h2 className='s-cols-2 m-cols-4 xl-cols-6'> Circulares</h2>      
 
+      <RecursosCard nombre="Circular AG 1" 
+      subt=" Registro de Dosis."
+      image="https://drive.google.com/uc?export=download&id=19FWbcWlGM6A44TFBPJalPasecf1x8vbJ"
+      link="https://drive.google.com/file/d/1esiwVnbMO3F2_Ro0CJF8rCO9r9zzQ_hs/view?usp=sharing"/>
+      
+      <RecursosCard nombre="Circular AG 2" 
+      subt="A partir del 31/03/2023."
+      image="https://drive.google.com/uc?export=download&id=1B3ztR66esBTIRLoFjhEBR8_Zk0Yl0kcH"
+      link="https://drive.google.com/file/d/1axHwL-uO-7fM1A-5Nac4dfGcY6f9IXJm/view?usp=sharing"/>
+      
+      <RecursosCard nombre="Circular AG 3" 
+      subt="A partir del 14/04/23."
+      image="https://drive.google.com/uc?export=download&id=1T46pJYuAJ13nFfvkT5e8YdFMjI7CJkb1"
+      link="https://drive.google.com/file/d/1buQRVoCnLAAtmz6FYoNdfe6jtyNDXgNS/view?usp=sharing"/>
+      
+      <RecursosCard nombre="Circular Refuerzos" 
+      subt="Indistintos +18 años."
+      image="https://drive.google.com/uc?export=download&id=1CL-Kg13P48PFuM-dqlV48sPVswzEW9KR"
+      link="https://drive.google.com/file/d/13RjWZ0ga2lyGzmqXtxIs4GWwdZ_9uqn5/view?usp=sharing"/>
+
       <RecursosCard nombre="Circular Refuerzos" 
       subt="6 Meses a 17 años"
       image="https://drive.google.com/uc?export=download&id=1-hnxlJpeDEoJ797qgqAKvUuKF9lVxu7s"
--- src/Components/Sections/slider.js
+++ src/Components/Sections/slider.js
@@ -3,30 +3,30 @@ import RecursosCard from "./Recursos-card"
 const Slider = () => (
      <div className="container-slider">
         <div className="slider " id="slider">
-
+       
         <div className="slider--section box-shadow">         
-        <RecursosCard nombre="Guia Rápida Vacunación" 
-        subt="Antigripal 2023"
-        image="https://drive.google.com/uc?export=download&id=1aBb12sp4g295DTszlyV7ax_zqS7KI0n0"
-        link="https://drive.google.com/file/d/1NLmybYs_pCinOpGKEF0n5LyRtlTDzj6g/view?usp=sharing"/>
+        <RecursosCard nombre="Circular AG 1" 
+        subt=" Registro de Dosis."
+        image="https://drive.google.com/uc?export=download&id=19FWbcWlGM6A44TFBPJalPasecf1x8vbJ"
+        link="https://drive.google.com/file/d/1esiwVnbMO3F2_Ro0CJF8rCO9r9zzQ_hs/view?usp=sharing"/>
         </div>
         <div className="slider--section box-shadow">         
-        <RecursosCard nombre="Manual Sputnik Ligth" 
-        subt="(Adultos) febrero 2023"
-        image="https://drive.google.com/uc?export=download&id=1XKy6Wc6eqk9u-g_eSBk6S-DvKFEROAdy"
-        link="https://drive.google.com/file/d/1rlf8AThX-uL07iR7y3SuovGUibpCWLqQ/view?usp=sharing"/>
+        <RecursosCard nombre="Circular AG 2" 
+        subt="A partir del 31/03/2023."
+        image="https://drive.google.com/uc?export=download&id=1B3ztR66esBTIRLoFjhEBR8_Zk0Yl0kcH"
+        link="https://drive.google.com/file/d/1axHwL-uO-7fM1A-5Nac4dfGcY6f9IXJm/view?usp=sharing"/>
         </div>
         <div className="slider--section box-shadow">         
-        <RecursosCard nombre="Almacenamiento Sputnik Ligth" 
-        subt=""
-        image="https://drive.google.com/uc?export=download&id=1dZVH8v7x-ROJxRY-AcW0Ims5QuvKSadi"
-        link="https://drive.google.com/file/d/11qggoSJgTk7gPhouBuQl5jKYalYQGQZ-/view?usp=sharing"/>
+        <RecursosCard nombre="Circular AG 3" 
+        subt="A partir del 14/04/23."
+        image="https://drive.google.com/uc?export=download&id=1T46pJYuAJ13nFfvkT5e8YdFMjI7CJkb1"
+        link="https://drive.google.com/file/d/1buQRVoCnLAAtmz6FYoNdfe6jtyNDXgNS/view?usp=sharing"/>
         </div>                
-        <div className="slider--section box-shadow">
-        <RecursosCard nombre="Manual Spikevax Bivariante" 
-        subt="Moderna Pediátrica"
-        image="https://drive.google.com/uc?export=download&id=1A4NcZTe-VOjyPzXlOkBgZuj-HxIOOjCm" 
-        link="https://drive.google.com/file/d/1c34APdiDiCOXGWea00hXunlC-oxKqWKJ/view?usp=sharing"/>
+        <div className="slider--section box-shadow">         
+        <RecursosCard nombre="Circular Refuerzos" 
+        subt="Indistintos +18 años."
+        image="https://drive.google.com/uc?export=download&id=1CL-Kg13P48PFuM-dqlV48sPVswzEW9KR"
+        link="https://drive.google.com/file/d/13RjWZ0ga2lyGzmqXtxIs4GWwdZ_9uqn5/view?usp=sharing"/>
         </div>
         
 
