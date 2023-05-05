bf92a5e81df85f14a908628e47b23e8d70c90d2c
Tav-115 <33943446+Tav-115@users.noreply.github.com>
2023-05-04 23:33:57 -0300

    4-5-23

--- src/Components/Sections/slider.js
+++ src/Components/Sections/slider.js
@@ -6,28 +6,31 @@ const Slider = () => (
         <div className="slider " id="slider">
        
         <div className="slider--section box-shadow">         
-        <RecursosCard nombre="Circular AG 1" 
-        subt=" Registro de Dosis."
-        image="https://drive.google.com/uc?export=download&id=19FWbcWlGM6A44TFBPJalPasecf1x8vbJ"
-        link="https://drive.google.com/file/d/1esiwVnbMO3F2_Ro0CJF8rCO9r9zzQ_hs/view?usp=sharing"/>
+        <RecursosCard nombre="Circular AG 4" 
+        subt="CONSENTIMIENTO RECHAZO DE VACUNACIÓN"
+        image="https://drive.google.com/uc?export=download&id=1DpjPfItNvBiQ15IxiKLwBhoF6DoL7yFb"
+        link="https://drive.google.com/file/d/1zkBm6QVxdfLekT5aVmgNfF0l_YG4AUEp/view?usp=sharing"/>
         </div>
+
         <div className="slider--section box-shadow">         
-        <RecursosCard nombre="Circular AG 2" 
-        subt="A partir del 31/03/2023."
-        image="https://drive.google.com/uc?export=download&id=1B3ztR66esBTIRLoFjhEBR8_Zk0Yl0kcH"
-        link="https://drive.google.com/file/d/1axHwL-uO-7fM1A-5Nac4dfGcY6f9IXJm/view?usp=sharing"/>
+        <RecursosCard nombre="Circular AG 5" 
+        subt="30/05/2023."
+        image="https://drive.google.com/uc?export=download&id=1VNqJeQogBljqxCUYO0wDlU4EIcsFEqsR"
+        link="https://drive.google.com/file/d/1RcuLGbp7geHrI8vjVxi9MK9GPZOvcmrf/view?usp=sharing"/>
         </div>
+                        
+        <div className="slider--section box-shadow">         
+        <RecursosCard nombre="Dosis de refuerzo contra COVID-19" 
+        subt="3-5-2023"
+        image="https://drive.google.com/uc?export=download&id=1Bq7Ve2Ir_fxjHN_pROrmnPEUUnKYURTi"
+        link="https://drive.google.com/file/d/1TqVShXC2a0oRbGPZx_TZsjpEDF6tF9-P/view?usp=sharing"/>
+        </div>
+
         <div className="slider--section box-shadow">         
         <RecursosCard nombre="Circular AG 3" 
         subt="A partir del 14/04/23."
         image="https://drive.google.com/uc?export=download&id=1T46pJYuAJ13nFfvkT5e8YdFMjI7CJkb1"
         link="https://drive.google.com/file/d/1buQRVoCnLAAtmz6FYoNdfe6jtyNDXgNS/view?usp=sharing"/>
-        </div>                
-        <div className="slider--section box-shadow">         
-        <RecursosCard nombre="Circular Refuerzos" 
-        subt="Indistintos +18 años."
-        image="https://drive.google.com/uc?export=download&id=1CL-Kg13P48PFuM-dqlV48sPVswzEW9KR"
-        link="https://drive.google.com/file/d/13RjWZ0ga2lyGzmqXtxIs4GWwdZ_9uqn5/view?usp=sharing"/>
         </div>
         
 
